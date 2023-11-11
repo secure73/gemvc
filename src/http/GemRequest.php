b43ae5688c9892857612461a1429618b18dd27a3
secure73 <secure73@gmail.com>
2023-11-11 13:27:45 +0100

    Update GemRequest.php

--- src/http/GemRequest.php
+++ src/http/GemRequest.php
@@ -2,6 +2,7 @@
 
 namespace GemLibrary\Http;
 
+use GemLibrary\Helper\JsonHelper;
 use GemLibrary\Helper\TypeHelper;
 
 class GemRequest
@@ -29,6 +30,7 @@ class GemRequest
 
     public function __construct()
     {
+        $this->error = "";
         $this->start_exec = microtime(true);
         $this->id = TypeHelper::guid();
         $this->time = TypeHelper::timeStamp();
@@ -48,17 +50,74 @@ class GemRequest
         return  $this->start_exec;
     }
 
+
+    public function validatePost(array $rules): bool
+    {
+        if(!$this->hasRequiredPost($rules))
+        {
+            return false;
+        }
+
+        foreach ($rules as $field => $rule) {
+            $rulesArr = explode('|', $rule);
+            foreach ($rulesArr as $r) {
+                if ($r === 'email' && !filter_var($this->post[$field], FILTER_VALIDATE_EMAIL)) {
+                    $this->error = "$field is not a valid email";
+                    return false;
+                }
+                if (strpos($r, 'min:') === 0 && strlen($this->post[$field]) < intval(substr($r, 4))) {
+                    $this->error = "$field must be at least " . substr($r, 4) . " characters";
+                    return false;
+                }
+
+                if (strpos($r, 'max:') === 0 && strlen($this->post[$field]) > intval(substr($r, 4))) {
+                    $this->error = "$field must be at most " . substr($r, 4) . " characters";
+                    return false;
+                }
+                if($r === 'int' && !is_numeric($this->post[$field]))
+                {
+                    $this->error = "$field must be an integer";
+                    return false;
+                }
+                if($r === 'float' && !is_float($this->post[$field]))
+                {
+                    $this->error = "$field must be a float";
+                    return false;
+                }
+                if($r === 'bool' && !is_bool($this->post[$field]))
+                {
+                    $this->error = "$field must be a boolean";
+                    return false;
+                }
+                if($r === 'array' && !is_array($this->post[$field]))
+                {
+                    $this->error = "$field must be an array";
+                    return false;
+                }
+                if($r === 'json' && !JsonHelper::validateJson($this->post[$field]))
+                {
+                    $this->error = "$field must be an object";
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    
    /**
     * @param array<string> $requieredPost
     */
-    public function hasRequiredPost(array $requieredPost): bool
+    private function hasRequiredPost(array $requieredPost): bool
     {
-        foreach ($requieredPost as $item) {
-            if (!isset($this->post[$item])) {
-                $this->error = "Missing required Post: $item";
+        foreach ($requieredPost as $key => $item) {
+            
+            if (!isset($this->post[$key])) {
+                $this->error = "post $key is required";
                 return false;
             }
         }
         return true;
     }
+
 }
