d515480400488a1ba5c358a068ffb0c4d6dd45f7
Roboxon <145359440+roboxon@users.noreply.github.com>
2024-01-20 23:49:28 +0100

    refactoring TokenModel

--- src/http/GemToken.php
+++ src/http/GemToken.php
@@ -3,26 +3,26 @@ namespace GemLibrary\Http;
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
-use GemLibrary\Helper\StringHelper;
-use GemLibrary\Helper\TypeHelper;
 
 class GemToken
 {
-    public string   $token_id;
+    public int       $exp;
+    public bool      $isTokenValid;
+    public int       $user_id;
+    public string    $type;
+    public array     $payload;/** @phpstan-ignore-line */
+    public ?string   $token_id;
     public ?string   $iss;
-    public int      $exp;
-    public bool     $isTokenValid;
-    public string   $type;
-    public array    $payload;/** @phpstan-ignore-line */
-    public int      $user_id;
-    public ?string  $error;
-    public ?string  $userMachine;
-    public ?string  $ip;
-    private string  $_secret;
+    public ?string   $role;
+    public ?int      $company_id;
+    public ?int      $employee_id;
+    public ?string   $error;
+    public ?string   $userAgent;
+    public ?string   $ip;
+    private string   $_secret;
 
     public function __construct(string $secret , string $issuer = null)
     {
-        $this->token_id = 'Not Initialized';
         $this->user_id = 0;
         if($issuer)
         {
@@ -46,27 +46,30 @@ class GemToken
      * @param null|string $userMachinToBeSensetive 
      * @return string
      */
-    public function create(string $type , int $userId, int $timeToLiveSecond, array $payload, string $ipAddressTobeSensitive = null, string $userMachinToBeSensetive = null): string
+    public function create(int $userId, int $timeToLiveSecond): string
     {
         $payloadArray = [
             'token_id' => microtime(true),
             'user_id' => $userId,
             'iss' => $this->iss,
             'exp' => (time() + $timeToLiveSecond),
-            'type' => $type,
-            'payload' => $payload
+            'type' => $this->type,
+            'payload' => $this->payload,
+            'role' => $this->role,
+            'company_id' => $this->company_id,
+            'employee_id' => $this->employee_id
         ];
-        return JWT::encode($payloadArray, $this->_generate_key($ipAddressTobeSensitive, $userMachinToBeSensetive), 'HS256');
+        return JWT::encode($payloadArray, $this->_generate_key(), 'HS256');
     }
 
     /**
      * @param string $token
      * @description pure token without Bearer you can use WebHelper::BearerTokenPurify() got get pure token
      */
-    public function validate(string $token,string $ip = null, string $userMachine = null): bool
+    public function verify(string $token): bool
     {
         try {
-            $decodedToken = JWT::decode($token, new Key($this->_generate_key($ip, $userMachine), 'HS256'));
+            $decodedToken = JWT::decode($token, new Key($this->_generate_key(), 'HS256'));
             if (isset($decodedToken->user_id) && $decodedToken->exp > time() && $decodedToken->user_id>0) {
                 $this->token_id = $decodedToken->token_id;
                 $this->user_id = (int)$decodedToken->user_id;
@@ -74,9 +77,10 @@ class GemToken
                 $this->iss = $decodedToken->iss;
                 $this->payload = $decodedToken->payload;
                 $this->isTokenValid = true;
-                $this->ip = $ip;
-                $this->userMachine = $userMachine;
                 $this->type = $decodedToken->type;
+                $this->role = $decodedToken->role;
+                $this->company_id = $decodedToken->company_id;
+                $this->employee_id = $decodedToken->employee_id;
                 $this->error = null;
                 return true;
             }
@@ -88,8 +92,8 @@ class GemToken
 
     public function renew(string $token, int $extensionTime_sec): false|string
     {
-        if ($this->validate($token, $this->ip, $this->userMachine)) {
-            return $this->create($this->type , $this->user_id, $extensionTime_sec, $this->payload ,$this->ip, $this->userMachine);
+        if ($this->verify($token)) {
+            return $this->create($this->user_id, $extensionTime_sec);
         }
         return false;
     }
@@ -124,8 +128,8 @@ class GemToken
      * @param string $machin
      * @return string
      */
-    private function _generate_key(string $ip = null, string $machin = null): string
+    private function _generate_key(): string
     {
-        return $this->_secret . $ip . $machin;
+        return $this->_secret . $this->ip . $this->userAgent;
     }
 }
